b06d9fb0e0fa596a662fe7cf2e1d6e0a657db838
lq0502 <148927487+lq0502@users.noreply.github.com>
2025-04-03 20:56:18 +0900

    Update Main.java

--- pet/src/Main.java
+++ pet/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // 启动UI
         javax.swing.SwingUtilities.invokeLater(() -> {
             Pet pet = PetDatabase.loadPetState();
             new PetUI(pet).createAndShowGUI();
